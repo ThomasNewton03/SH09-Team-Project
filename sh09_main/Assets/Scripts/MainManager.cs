fe1e29973040c3dfd0c0e611fa16386f79e93aff
2662611d <2662611d@student.gla.ac.uk>
2023-11-15 15:24:53 +0000

    Added Data Peristence
    -Data persists for both changes in scene and for changes in session
    -Implemented using JSON Serializability
    
    MainManager is an example of a persisting class that is attached to an
    empty object of the same name.
    
    Awake() is called on when the object the
    script is attached to is initialised or set to active. The current Awake
    method confirms the class is singleton across all scenes using a stored
    instance of MainManager by using the unity method DontDestroyOnLoad().
    
    The Class SaveData stores the data to be saved, marke by Serializable to
    allow JSON parsing.
    
    SaveSettings() stores the SaveData variables in a JSON file,
    Application.persistentDataPath is a different location on different
    systems.
    
    LoadSettings() gets the path and if exists it fetches the data and
    stores into the according SaveData variables.

---
+++ sh09_main/Assets/Scripts/MainManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    public int fontSize;
+
+    private void Awake() 
+    {
+        if (Instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadSettings();
+    } 
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int fontSize;
+    }
+
+    public void SaveSettings()
+    {
+        SaveData data = new SaveData();
+        data.fontSize = fontSize;
+
+        string json = JsonUtility.ToJson(data);
+    
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadSettings()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            fontSize = data.fontSize;
+        }
+    }
+
+    
+}
