0ec9a020116f6b2a7557e7665eced3bd626ee63e
Josh Duffy <2662611d@student.gla.ac.uk>
2023-11-22 16:34:53 +0000

    Update Game.cs

--- sh09_main/Assets/Scripts/Game.cs
+++ sh09_main/Assets/Scripts/Game.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 public class Game : MonoBehaviour{
+    public void Start(){
+        GameObject obj = GameObject.Find("Settings");
+        Debug.Log(obj.name);
+        // obj.GetComponent<Settings>();
+        
+    }
 
+    public Game(){
+    }
 
 
 }
