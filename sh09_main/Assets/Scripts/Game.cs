296fa177e1dc9ca2984b1210e347c5b2cf804221
Aiden Smith <2660428s@student.gla.ac.uk>
2023-11-22 19:54:50 +0000

    Added the c# scripts to unity, so the base scripts are set up

--- sh09_main/Assets/Scripts/Game.cs
+++ sh09_main/Assets/Scripts/Game.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 public class Game : MonoBehaviour{
 
 
+    Inventory inventory;
+    
 
+    public void addGundamOnDiscovery(string gundamRobot){
+        inventory.addToInventory(gundamRobot);
+    }
 
+}
