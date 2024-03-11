73594ea13eacc7518d1be04b38005e417f908091
Thomas Newton <tjnewton2003@gmail.com>
2024-03-11 13:25:01 +0000

    The CheckBoundary.cs script has been added. This will allow a pop up message to appear when a user has reached a certain distance from the model after collecting it. It does this by checking the distance between the user's current location and their location when they collected the model. It compares this distance to a threshold maximum distance value.

---
+++ sh09_main/Assets/Scripts/CheckBoundary.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+using Mapbox.Unity.Location;
+using Mapbox.Utils;
+
+public class CheckBoundary : AbstractLocationProvider {
+
+    //user location is assumed to be at a Gundam model
+    public Vector2d userLocation;
+
+    void Start(){
+
+        OnLocationUpdated += OnLocationUpdatedHandler;
+
+    }
+
+
+
+    void OnLocationUpdatedHandler(Location location){
+        //checks the distance of the coordinates and ensures the user is within range
+		float maxDistance = 0.00030f;
+        userLocation = location.LatitudeLongitude;
+
+        float distanceFromGundam = (float)Vector2d.Distance(userLocation, _currentLocation.LatitudeLongitude);
+
+		if (distanceFromGundam > maxDistance){
+			Debug.Log("You are out of bounds, please turn around!");
+		}
+    }
+
+}
